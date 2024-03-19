fc8233d0fafb1c83e5a98640eb2374d873fbd76b
tamilselvan772 <111485180+tamilselvan772@users.noreply.github.com>
2024-03-19 22:37:54 +0530

    Delete 3d_shape directory

--- 3d_shape/cuboid.js
+++
@@ -1,9 +0,0 @@
-var l = parseInt(prompt("enter the l"));
-var b = parseInt(prompt("enter the b"));
-var h = parseInt(prompt("enter the height"));
-var volume = l * b * h;
-var total = 2 * h * (l + b);
-var area = 2 * (l * b + b * h + l * h);
-document.write("cuboid volume " + volume + "<br>");
-document.write("cuboid total surface area " + total + "<br>");
-document.write("cuboid curved  surface area " + area + "<br>");
--- 3d_shape/hemisphere.js
+++
@@ -1,7 +0,0 @@
-var r = parseInt(prompt("enter the r"));
-var volume = (2 / 3) * 3.14 * r * r * r;
-var total = 3 * 3.14 * r * r;
-var area = 2 * 3.14 * r * r;
-document.write("hemisphere volume" + volume + "<br>");
-document.write("hemisphere total surface area" + total + "<br>");
-document.write("hemisphere curved surface area" + area + "<br>");
