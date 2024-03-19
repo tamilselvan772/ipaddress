ad6f5296e18b6d3eb04b06113507a3ac1a62f705
tamilselvan772 <111485180+tamilselvan772@users.noreply.github.com>
2024-03-19 22:39:12 +0530

    Delete BMI directory

--- BMI/script.js
+++
@@ -1,6 +0,0 @@
-function bmi() {
-  var a = parseInt(document.getElementById("weight").value);
-  var b = parseInt(document.getElementById("height").value);
-  var total = (kg / m) * m;
-  document.getElementById("root").innerHTML = "Total Surface Area : " + total;
-}
